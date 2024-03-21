c9721091a7eb63dda88a7a4adfb59e3999a2927b
David Liva <davidliva01@gmail.com>
2024-03-20 22:17:54 -0300

    pequeñas modificaciones de identación

--- Clase-4/public/js/app.js
+++ Clase-4/public/js/app.js
@@ -11,12 +11,6 @@ ElButton.addEventListener('click', () => {
     let col_2 = document.getElementById('col-2').value
 
 
-    
-    // console.log(parseInt(raw_1) + parseInt(col_1))
-    // console.log(parseInt(raw_2) + parseInt(col_2))
-
-    // let array2 = document.getElementById('col-1').value
-
     if((raw_1 == '' || raw_2 == '')&& (col_1 == '' || col_2 == '')){
         console.log('Rellene todos los campos')
         const message = document.getElementById('message')
@@ -32,20 +26,6 @@ ElButton.addEventListener('click', () => {
 
         array1.push(parseInt(raw_1), parseInt(raw_2))
         array2.push(parseInt(col_1), parseInt(col_2))
-
-        // for (let sumArray = []; ((array1 != '' || undefined)&(array2 != '' || undefined)) & (length.sumArray < 5); [...sumArray, array1, array2]) {
-        //     // const element = array[valorActual];
-        //     console.log(sumArray)
-            
-        // }
-        // const sumArreglo = (elemento, indice, array1) => {
-        //     elemento 
-        // }
-        // while(sumArray.length < 1) {
-        // let longSumArray = sumArray.push(array1, array2)
-        // console.log('Longitud del nuevo array: ',longSumArray)
-        // console.log('Suma total de ambos arrays: ', sumArray)
-        // }
 
         if(array1.length === array2.length){
             let sumArray = [];
@@ -83,19 +63,10 @@ ElButton.addEventListener('click', () => {
             console.log('Los arrays no tienen la misma longitud')
         }
 
-
-
-
-
         console.log('Valores del primer array: ', array1)
         console.log('Valores del segundo array: ', array2)
-        // console.log('La longitud del primer array es de: ',long1)
-        // console.log('Valores del segundo array: ', array2)
 
     }
-
-    
-
 
     
 })
