a09b6e043c7339a4c95caf45ad3d15a900f03a4b
David Liva <livadavid78@gmail.com>
2024-05-08 16:08:00 -0300

    Se han hecho unas pequeñas modificaciones de visualizacion al modelo de toxicidad

--- Clase-13/app.js
+++ Clase-13/app.js
@@ -7,17 +7,18 @@ evaluar.addEventListener('click', () => {
 
     const elText = document.getElementById('text').value
     const elMessage = document.getElementById('message')
+    const results = document.getElementById('results')
+    results.innerHTML = ''
     document.getElementById('text').value = ''
 
     elMessage.style.fontFamily = 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode';
     elMessage.style.margin = '10px'
     elMessage.textContent = `El mensaje es:  ${elText}`;
     
-    toxicity.load(treshold).then(model => {
-        const sentences = [elText];
-
-        model.classify(sentences)
-        .then(predictions => {
+    toxicity.load(treshold)
+    .then(model => { const sentences = [elText];
+     model.classify(sentences)
+    .then(predictions => {
             
             console.log(JSON.stringify(predictions, null, 2))
             
@@ -28,11 +29,13 @@ evaluar.addEventListener('click', () => {
                 const elColor = document.createElement('div');
 
                 const primerResultado = element.results[0];
-                console.log("variable1: ",primerResultado)
+                console.log("Resultados de la predicción: ",primerResultado)
+                
                 const probabilidades = primerResultado.probabilities;
-                console.log("variablle2: ", probabilidades)
+                console.log("Pobabilidades: ", probabilidades)
 
-                const maxcolor = probabilidades[0] * 100
+                let maxcolor = probabilidades[0] * 100
+                maxcolor = parseInt(maxcolor)
                 
                 elColor.style.maxWidth = `${maxcolor}%`;
                 elColor.style.backgroundColor = '#f00';
@@ -50,7 +53,6 @@ evaluar.addEventListener('click', () => {
                 
                 elResults.appendChild(elDiv)
                 
-                console.log("este es el resultado:",element)
             });
         })
     })
